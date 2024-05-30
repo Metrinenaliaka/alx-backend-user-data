8856f2383e2176135c6b42e2ae636baa3f3fec1f
Metrinenaliaka <metrinenam@gmail.com>
2024-05-30 14:27:31 +0300

    using bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+encypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password):
+    """
+    a function that hashes a password
+    """
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the generated salt
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed_password
