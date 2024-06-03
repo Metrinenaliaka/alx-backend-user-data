9e4e9103aa52d4a6d8e4003320dd27b683847247
Metrinenaliaka <metrinenam@gmail.com>
2024-06-03 13:27:13 +0300

    implementing Basic auth

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+handling authentication for the API
+"""
+
+from typing import TypeVar
+from flask import request, jsonify, abort
+from typing import List, TypeVar
+import fnmatch
+
+
+class Auth:
+    """
+    class handling auth
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        required paths"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        # if path in excluded_paths:
+        #     return False
+        if path[-1] != '/':
+            path += '/'
+        # if path not in excluded_paths:
+        #     return True
+        for match in excluded_paths:
+            if fnmatch.fnmatch(path, match):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        authorization header
+        """
+        if request is not None:
+            return request.headers.get('Authorization')
+
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current user
+        """
+        return None
