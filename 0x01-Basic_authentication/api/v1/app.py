bec6113d4565bd61d63c008bc357458ee58cda1a
Metrinenaliaka <metrinenam@gmail.com>
2024-06-04 14:19:42 +0300

    fixed circular import

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -11,7 +11,7 @@ import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv('AUTH_TYPE') == 'auth':
