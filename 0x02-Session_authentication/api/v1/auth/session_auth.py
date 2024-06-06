31f5b9a3f0e14017d4153d728692029d3be19350
Metrinenaliaka <metrinenam@gmail.com>
2024-06-06 13:40:37 +0300

    added session creation

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 handling authentication for the API
 """
 import os
+import uuid
 from api.v1.auth.auth import Auth
 
 os.environ['AUTH_TYPE'] = 'SessionAuth'
@@ -12,4 +13,14 @@ class SessionAuth(Auth):
     """
     class for session authentication
     """
-    pass
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
