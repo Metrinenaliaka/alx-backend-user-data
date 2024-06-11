7974ec77ed9fba14d554a8419d848911a0e44b0d
Metrinenaliaka <metrinenam@gmail.com>
2024-06-11 13:48:47 +0300

    authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,20 @@ def logout() -> Response:
         return jsonify({"message": "Forbidden"}), 403
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> Response:
+    """
+    gets user profile
+    """
+    session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        return jsonify({"message": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
