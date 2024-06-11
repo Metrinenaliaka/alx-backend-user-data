780e079efded3e58cc669d4ccec2d94a1e5b9b24
Metrinenaliaka <metrinenam@gmail.com>
2024-06-11 13:52:30 +0300

    resetting password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,8 +59,8 @@ class Auth:
             stored_hashed_password = user.hashed_password
             password_bytes = password.encode('utf-8')
             return bcrypt.checkpw(password_bytes, stored_hashed_password)
-        except NoResultFound:
-            return False
+        except NoResultFound as exc:
+            raise ValueError from exc
 
     def create_session(self, email: str) -> str:
         """
@@ -88,3 +88,16 @@ class Auth:
         destroys a session
         """
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        generates a reset token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound as exc:
+            raise ValueError from exc
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
