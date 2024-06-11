1f0a1ddd46e6b79f294380b5bb0e7b3099f00d80
Metrinenaliaka <metrinenam@gmail.com>
2024-06-11 14:36:38 +0300

    main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,23 +2,133 @@
 """
 Main file
 """
-from db import DB
-from user import User
-
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+import requests
 
 
-my_db = DB()
+def register_user(email: str, password: str) -> None:
+    """
+    checks register user response
+    """
+    url = "http://localhost:5000/register"
+    data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post(url, json=data)
+    print(response.status_code)
+    assert response.status_code == 200
+    print("User registered successfully.")
 
-email = 'test@test.com'
-hashed_password = "hashedPwd"
 
-user = my_db.add_user(email, hashed_password)
-print(user.id)
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    checks login response with wrong password
+    """
+    url = "http://localhost:5000/login"
+    data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post(url, json=data)
+    assert response.status_code == 401
+    print("Login failed with wrong password.")
 
-try:
-    my_db.update_user(user.id, hashed_password='NewPwd')
-    print("Password updated")
-except ValueError:
-    print("Error")
+
+def log_in(email: str, password: str) -> str:
+    """
+    checks login response with correct password
+    """
+    url = "http://localhost:5000/login"
+    data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+    session_id = response.json()["session_id"]
+    print("Logged in successfully.")
+    return session_id
+
+
+def profile_unlogged() -> None:
+    """
+    checks profile response for unlogged user
+    """
+    url = "http://localhost:5000/profile"
+    response = requests.get(url)
+    assert response.status_code == 403
+    print("Access to profile denied for unlogged user.")
+
+
+def profile_logged(session_id: str) -> None:
+    """
+    checks profile response for logged user
+    """
+    url = "http://localhost:5000/profile"
+    headers = {
+        "session_id": session_id
+    }
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200
+    print("Access to profile granted for logged user.")
+
+
+def log_out(session_id: str) -> None:
+    """
+    checks logout response
+    """
+    url = "http://localhost:5000/logout"
+    headers = {
+        "session_id": session_id
+    }
+    response = requests.delete(url, headers=headers)
+    assert response.status_code == 200
+    print("Logged out successfully.")
+
+
+def reset_password_token(email: str) -> str:
+    """
+    checks reset password token response
+    """
+    url = "http://localhost:5000/reset_password"
+    data = {
+        "email": email
+    }
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+    reset_token = response.json()["reset_token"]
+    print("Reset password token generated.")
+    return reset_token
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """
+    checks update password response
+    """
+    url = "http://localhost:5000/update_password"
+    data = {
+        "email": email,
+        "reset_token": reset_token,
+        "new_password": new_password
+    }
+    response = requests.put(url, json=data)
+    assert response.status_code == 200
+    print("Password updated successfully.")
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
